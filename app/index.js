3f5a2c80e8de183677e14b8c2b106d275ec91546
Randell Dawson <randy@randydawson.com>
2022-08-05 18:11:27 -0600

    fix: testing out adding hostname automatically

--- app/index.js
+++ app/index.js
@@ -12,7 +12,9 @@ app.use(cors());
 app.use('/public', express.static(`${process.cwd()}/app/public`));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (_, res) => {
+app.get('/', (req, res) => {
+  console.log('logging hostname')
+  console.log(req.hostname)
   res.sendFile(process.cwd() + '/app/views/index.html');
 });
 
